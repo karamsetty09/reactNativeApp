0d803c1a6c541abb3ae1d0a20e15253e70e10f0f
venkata <sandeepsandy2530@gmail.com>
2022-04-12 16:54:27 +1000

    Navigation in reactnative with screens.

--- App.js
+++ App.js
@@ -1,55 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, SafeAreaView, FlatList } from 'react-native';
-import ColorBox from './components/ColorBox';
+import React from 'react';
+import Home from './screens/Home';
+import ColorPalette from './screens/ColorPalette';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
 
 export default function App() {
-  const COLORS = [
-    { colorName: 'Base03', hexCode: '#002b36' },
-    { colorName: 'Base02', hexCode: '#073642' },
-    { colorName: 'Base01', hexCode: '#586e75' },
-    { colorName: 'Base00', hexCode: '#657b83' },
-    { colorName: 'Base0', hexCode: '#839496' },
-    { colorName: 'Base1', hexCode: '#93a1a1' },
-    { colorName: 'Base2', hexCode: '#eee8d5' },
-    { colorName: 'Base3', hexCode: '#fdf6e3' },
-    { colorName: 'Yellow', hexCode: '#b58900' },
-    { colorName: 'Orange', hexCode: '#cb4b16' },
-    { colorName: 'Red', hexCode: '#dc322f' },
-    { colorName: 'Magenta', hexCode: '#d33682' },
-    { colorName: 'Violet', hexCode: '#6c71c4' },
-    { colorName: 'Blue', hexCode: '#268bd2' },
-    { colorName: 'Cyan', hexCode: '#2aa198' },
-    { colorName: 'Green', hexCode: '#859900' },
-  ];
   return (
-    <SafeAreaView >
-      <FlatList
-          style={styles.container}
-          data={COLORS}
-          keyExtractor={item=>item.colorName}
-          renderItem={({item}) => <ColorBox colorName={item.colorName} hexCode={item.hexCode}/>}
-          ListHeaderComponent={<Text style={styles.text}>Solarized</Text>}
-        />
-      <View style={styles.container}>
-        <Text style={styles.text}>Here are some boxes of different colours</Text>
-        <ColorBox colorName="cyan" hexCode="#2aa198"/>
-        <ColorBox colorName="blue" hexCode="#268bd2"/>
-        <ColorBox colorName="magenta" hexCode="#d33682"/>
-        <ColorBox colorName="orange" hexCode="#cb4b16"/>
-      </View>
-   </SafeAreaView>
-
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="ColorPalette" component={ColorPalette} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    paddingHorizontal: 10,
-    paddingTop: 40,
-  },
-  text: {
-    fontSize: 18,
-    fontWeight: 'bold',
-    marginBottom: 10,
-  },
-});
