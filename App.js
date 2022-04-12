12b0a520534ee52b3ab4a068ffc18b8b084386c0
venkata <sandeepsandy2530@gmail.com>
2022-04-12 14:40:10 +1000

    Designing in react native.

--- App.js
+++ App.js
@@ -3,9 +3,21 @@ import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 
 export default function App() {
   return (
-    <SafeAreaView style={styles.safeArea}>
-      <View style={[styles.container, styles.teal]}>
-        <Text>Hello world !!!</Text>
+    <SafeAreaView >
+      <View style={styles.container}>
+        <Text style={styles.text}>Here are some boxes of different colours</Text>
+        <View style={[styles.cyan, styles.box]}>
+          <Text style={styles.boxText}>Cyan #2aa198</Text>
+        </View>
+        <View style={[styles.blue, styles.box]}>
+          <Text style={styles.boxText}>Blue #268bd2</Text>
+        </View>
+        <View style={[styles.magenta, styles.box]}>
+          <Text style={styles.boxText}>Magento #d33682</Text>
+        </View>
+        <View style={[styles.orange, styles.box]}>
+          <Text style={styles.boxText}>Orange #cb4b16</Text>
+        </View>
       </View>
    </SafeAreaView>
 
@@ -13,16 +25,36 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
-  teal: {
-    backgroundColor: 'pink',
-  },
   container: {
-    margin: 10,
+    paddingHorizontal: 10,
+    paddingTop: 40,
+  },
+  text: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  cyan: {
+    backgroundColor: '#2aa198',
+  },
+  blue: {
+    backgroundColor: '#268bd2',
+  },
+  magenta: {
+    backgroundColor: '#d33682',
+  },
+  orange: {
+    backgroundColor: '#cb4b16',
+  },
+  box: {
+    padding: 10,
+    // below two for making center.
     alignItems: 'center',
     justifyContent: 'center',
-    flex: 1,
+    marginBottom: 10,
   },
-  safeArea: {
-    flex: 2,
+  boxText: {
+    color: 'white',
+    fontWeight: 'bold',
   }
 });
