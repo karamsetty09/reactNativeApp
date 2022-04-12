dee3883f2b505b2f725ab66e1fcddccf0400c92b
venkata <sandeepsandy2530@gmail.com>
2022-04-12 16:07:16 +1000

    Always use flatlist or sectionlist in react native.

--- components/ColorBox.js
+++ components/ColorBox.js
@@ -4,10 +4,13 @@ import {View, Text, StyleSheet } from 'react-native';
 const ColorBox = ({colorName, hexCode}) => {
     const boxColor = {
         backgroundColor: hexCode,
-    }
+    };
+    const textColor = {
+        color: parseInt(hexCode.replace('#', ''), 16) > 0xffffff / 1.1 ? 'black' : 'white',
+    };
     return(
         <View style={[boxColor, styles.box]}>
-          <Text style={styles.boxText}>
+          <Text style={[styles.boxText, textColor]}>
               {colorName}: {hexCode}
             </Text>
         </View>
