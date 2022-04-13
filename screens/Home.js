3a02f701d9e17e1d5cb7c3c0279f80ec7d1148d6
venkata <sandeepsandy2530@gmail.com>
2022-04-13 13:18:23 +1000

    fetch from API and loading on application.

--- screens/Home.js
+++ screens/Home.js
@@ -1,56 +1,39 @@
-import React from 'react';
-import { FlatList, StyleSheet, SafeAreaView, } from 'react-native';
+import React, {useState, useCallback, useEffect} from 'react';
+import { FlatList, StyleSheet, SafeAreaView } from 'react-native';
 import PalettePreview from '../components/PalettePreview';
 import Counter from '../components/Counter';
 import FetchByUseEffect from '../components/FetchByUseEffect';
 
-const SOLARIZED = [
-    { colorName: 'Base03', hexCode: '#002b36' },
-    { colorName: 'Base02', hexCode: '#073642' },
-    { colorName: 'Base01', hexCode: '#586e75' },
-    { colorName: 'Base00', hexCode: '#657b83' },
-    { colorName: 'Base0', hexCode: '#839496' },
-    { colorName: 'Base1', hexCode: '#93a1a1' },
-    { colorName: 'Base2', hexCode: '#eee8d5' },
-    { colorName: 'Base3', hexCode: '#fdf6e3' },
-    { colorName: 'Yellow', hexCode: '#b58900' },
-    { colorName: 'Orange', hexCode: '#cb4b16' },
-    { colorName: 'Red', hexCode: '#dc322f' },
-    { colorName: 'Magenta', hexCode: '#d33682' },
-    { colorName: 'Violet', hexCode: '#6c71c4' },
-    { colorName: 'Blue', hexCode: '#268bd2' },
-    { colorName: 'Cyan', hexCode: '#2aa198' },
-    { colorName: 'Green', hexCode: '#859900' },
-  ];  
-
-const RAINBOW = [
-    { colorName: 'Red', hexCode: '#FF0000' },
-    { colorName: 'Orange', hexCode: '#FF7F00' },
-    { colorName: 'Yellow', hexCode: '#FFFF00' },
-    { colorName: 'Green', hexCode: '#00FF00' },
-    { colorName: 'Violet', hexCode: '#8B00FF' },
-  ];
-  
-const FRONTEND_MASTERS = [
-    { colorName: 'Red', hexCode: '#c02d28' },
-    { colorName: 'Black', hexCode: '#3e3e3e' },
-    { colorName: 'Grey', hexCode: '#8a8a8a' },
-    { colorName: 'White', hexCode: '#ffffff' },
-    { colorName: 'Orange', hexCode: '#e66225' },
-  ];
-
-const COLOR_PALETTES = [
-    { paletteName: 'Solarized', colors: SOLARIZED },
-    { paletteName: 'Frontend Masters', colors: FRONTEND_MASTERS },
-    { paletteName: 'Rainbow', colors: RAINBOW },
-];
-
 const Home = ({navigation}) => {
+    const [colorPalettes, setColorPalettes] = useState([]);
+    const [refreshing, setRefreshing] = useState(false);
+
+    const onRefresh = useCallback(() => {
+        setRefreshing(true);
+        fetchColors();
+        // intentionally setting timeout for display.
+        setTimeout(()=>{setRefreshing(false);},1000)
+        
+    }, []);
+
+    const fetchColors = useCallback(async () => {
+        const result = await fetch('https://color-palette-api.kadikraman.vercel.app/palettes');
+        
+        if(result.ok){
+            const palettes = await result.json();
+            setColorPalettes(palettes);
+        }
+    }, []);
+
+    useEffect(() => {
+        fetchColors()
+    },[]);
+
     return (
         <SafeAreaView>
             <FlatList
                 style={styles.list}
-                data={COLOR_PALETTES}
+                data={colorPalettes}
                 keyExtractor={item => item.paletteName}
                 renderItem={({ item }) => (
                     <PalettePreview
@@ -60,9 +43,12 @@ const Home = ({navigation}) => {
                         colorPalette={item}
                     />
                 )}
+               
+                refreshing={refreshing}
+                onRefresh ={onRefresh}      
+
             />
             <Counter />
-            
             <FetchByUseEffect style={styles.box}/>
         </SafeAreaView>
     )
